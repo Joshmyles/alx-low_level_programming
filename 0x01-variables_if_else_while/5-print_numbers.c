c916b968ac6603fd33cd3dcb806053a622e20221
Joshmyles <joshmyles844@gmail.com>
2023-09-08 20:01:05 -0700

    new changes

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*comment*/int main(void)/*comment*/
+{
+for (int i = 0; i <= 9; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*comment*/int main(void)/*comment*/
+{
+putchar('0');
+putchar('\n');
+putchar('1');
+putchar('\n');
+putchar('2');
+putchar('\n');
+putchar('3');
+putchar('\n');
+putchar('4');
+putchar('\n');
+putchar('5');
+putchar('\n');
+putchar('6');
+putchar('\n');
+putchar('7');
+putchar('\n');
+putchar('8');
+putchar('\n');
+putchar('9');
+putchar('\n');
+return (0);
+}
