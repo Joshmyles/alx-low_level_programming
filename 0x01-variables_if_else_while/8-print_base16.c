38bcac7989f5a82407feca55da5da1a03fc4422c
Joshmyles <joshmyles844@gmail.com>
2023-09-10 06:56:55 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,11 +2,7 @@
 
 /*comment*/int main(void)/*comment*/
 {
-char hexDigits[] = "0123456789abcdef"; /*Array of hexadecimal digits*/
-for (int i = 0; i < 16; i++) /*Loop through the hexadecimal digits*/
-{
-putchar(hexDigits[i]); /*Print the current digit*/
-}
-putchar('\n'); /*Print a newline character*/
+char hexDigits[] = "0123456789abcdef";
+printf("%s\n", hexDigits);
 return (0);
 }
