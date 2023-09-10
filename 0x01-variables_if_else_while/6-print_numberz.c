7c1be2aaec2d048dfb8cb3c26f302d9e69939561
Joshmyles <joshmyles844@gmail.com>
2023-09-10 06:33:34 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,25 +2,12 @@
 
 /*comment*/int main(void)/*comment*/
 {
-putchar('0');
-putchar('\n');
-putchar('1');
-putchar('\n');
-putchar('2');
-putchar('\n');
-putchar('3');
-putchar('\n');
-putchar('4');
-putchar('\n');
-putchar('5');
-putchar('\n');
-putchar('6');
-putchar('\n');
-putchar('7');
-putchar('\n');
-putchar('8');
-putchar('\n');
-putchar('9');
-putchar('\n');
+int i;
+for (i = 0; i < 10; ++i)
+{
+printf("%d\n", i);
+}
+printf("\n");
 return (0);
 }
+
