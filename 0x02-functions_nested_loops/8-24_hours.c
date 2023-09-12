ca436cf45854b0e3655094ce22539373ece9a085
Joshmyles <joshmyles844@gmail.com>
2023-09-12 16:35:50 -0700

    number 7

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,14 +2,16 @@
 
 /*comment*/void jack_bauer(void)/*comment*/
 {
-    for (int hour = 0; hour < 24; hour++) {
-        for (int minute = 0; minute < 60; minute++) {
-            _putchar('0' + (hour / 10));       
-            _putchar('0' + (hour % 10));
-            _putchar(':');
-	    _putchar('0' + (minute / 10));
-            _putchar('0' + (minute % 10));
-            _putchar('\n');
-        }
-    }
+for (int hour = 0; hour < 24; hour++)
+{
+for (int minute = 0; minute < 60; minute++)
+{
+_putchar('0' + (hour / 10));
+_putchar('0' + (hour % 10));
+_putchar(':');
+_putchar('0' + (minute / 10));
+_putchar('0' + (minute % 10));
+_putchar('\n');
+}
+}
 }
