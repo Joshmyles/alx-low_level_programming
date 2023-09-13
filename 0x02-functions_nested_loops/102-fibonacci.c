0fbf4a200d9bb65503fef184fd569670d4a8266a
Joshmyles <joshmyles844@gmail.com>
2023-09-12 22:48:25 -0700

    number 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*comment*/int main(void)/*comment*/
+{
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+for (count = 0; count < 50; count++)
+{
+sum = fib1 + fib2;
+printf("%lu", sum);
+fib1 = fib2;
+fib2 = sum;
+if (count == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
