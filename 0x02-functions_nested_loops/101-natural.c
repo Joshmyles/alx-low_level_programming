df10d1a49ee54d70ca6fc8e4f5195bfbb3cbc962
Joshmyles <joshmyles844@gmail.com>
2023-09-12 22:45:09 -0700

    number 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*comment*/int main()/*comment*/
+/*comment*/int main(void)/*comment*/
 {
-int sum = 0;
-for (int i = 3; i < 1024; i++)
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
 {
-if (i % 3 == 0 || i % 5 == 0)
-{
+if ((i % 3) == 0 || (i % 5) == 0)
 sum += i;
-}
 }
 printf("%d\n", sum);
 return (0);
