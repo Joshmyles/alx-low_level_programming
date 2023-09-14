554bfa390b150412f5a802eb3d60715a9787d499
Joshmyles <joshmyles844@gmail.com>
2023-09-14 07:59:20 -0700

    number 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - leave 2 and 4
+ * Return: all the numbers but 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i == 2 || i == 4)
+{
+continue;
+}
+else
+{
+_putchar (i);
+}
+}
+}
