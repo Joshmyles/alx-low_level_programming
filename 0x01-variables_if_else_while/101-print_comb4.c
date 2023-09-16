0390f3fa6837b515a9810732ea180b845c9d7fe3
Joshmyles <joshmyles844@gmail.com>
2023-09-16 00:30:23 -0700

    11. The success combination in business is: Do what you do better.. and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
-/*comment*/int main(void)/*comment*/
+/**
+ * main - main function
+ * Return: a combination of smallest numbers
+ */
+
+int main(void)
 {
-for (int i = 0; i <= 7; i++) // Loop through the first digit (0 to 7)
+for (int i = 0; i <= 7; i++)
 {
-for (int j = i + 1; j <= 8; j++) // Loop through the second digit (i+1 to 8)
+for (int j = i + 1; j <= 8; j++)
 {
-for (int k = j + 1; k <= 9; k++) // Loop through the third digit (j+1 to 9)
+for (int k = j + 1; k <= 9; k++)
 {
-putchar(i + '0'); // Print the first digit as a character
-putchar(j + '0'); // Print the second digit as a character
-putchar(k + '0'); // Print the third digit as a character
-if (i < 7) // Check if it's not the second-to-last combination (i < 7)
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i < 7)
 {
-putchar(','); // Print a comma
-putchar(' '); // Print a space
+putchar(',');
+putchar(' ');
 }
 }
 }
 }
-putchar('\n'); // Print a newline character
+putchar('\n');
 return (0);
 }
