c0d3878b6a2938e51a35915b8ec9ab66b8e7ea68
Joshmyles <joshmyles844@gmail.com>
2023-09-16 00:23:54 -0700

    9. Patience, persistence and perspiration make an unbeatable comibination for success

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-/*comment*/int main(void)/*comment*/
+/**
+ * main - main function
+ * Return: Number combination
+ */
+
+int main(void)
 {
-for (int i = 0; i <= 9; i++) // Loop through the first digit (0 to 9)
+int i, j;
+for (i = 0; i <= 9; i++)
 {
-for (int j = i + 1; j <= 9; j++) // Loop through the second digit (i+1 to 9)
+for (j = i + 1; j <= 9; j++)
 {
-putchar(i + '0'); // Print the first digit as a character
-putchar(j + '0'); // Print the second digit as a character
-if (i < 8) // Check if it's not the second-to-last digit (i < 8)
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8)
 {
-putchar(','); // Print a comma
-putchar(' '); // Print a space
+putchar(',');
+putchar(' ');
 }
 }
 }
-putchar('\n'); // Print a newline character
+putchar('\n');
 return (0);
 }
