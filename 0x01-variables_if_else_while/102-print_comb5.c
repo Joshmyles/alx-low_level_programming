f994061a5d4aef375670e3b491562d38e4e7ce3b
Joshmyles <joshmyles844@gmail.com>
2023-09-16 01:39:52 -0700

    12. Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - main function
- * Return: a two pair combination
+ * main - function
+ * Return: something
  */
 
 int main(void)
 {
 int i, j;
-for (i = 0; i <= 99; i++)
+for (i = 0; i < 100; i++)
 {
-for (j = i; j <= 99; j++)
+for (j = i; j < 100; j++)
 {
-/*print first two digit numbers with 0 if necessary*/
-if (i < 10)
-{
-putchar('0');
-}
-putchar(i + '0');
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-/*Print the second two-digit number with leading zero if necessary*/
-if (j < 10)
-{
-putchar('0');
-}
-putchar(j + '0');
-/*check if it is not 99*/
-if (!(i == 99 && j == 99))
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 99 || j != 99)
 {
 putchar(',');
 putchar(' ');
