523d801209e3b52c40fb4586c24e53ef72ec1f25
Joshmyles <joshmyles844@gmail.com>
2023-09-16 01:43:40 -0700

    12. Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,6 +12,8 @@ for (i = 0; i < 100; i++)
 {
 for (j = i; j < 100; j++)
 {
+if (i != j)
+{
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
@@ -21,6 +23,7 @@ if (i != 99 || j != 99)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
