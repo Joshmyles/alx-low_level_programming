1a8245360510866631c6e3b63829f3e90ba8056c
Joshmyles <joshmyles844@gmail.com>
2023-09-15 23:36:22 -0700

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,8 +8,7 @@
 int main(void)
 {
 int i;
-char hexDigits[] = 16; 
-hexDigits = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+char hexDigits[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 for (i = 0; i < hexDigits.length; i++)
 {
 putchar(hexDigits[i]);
