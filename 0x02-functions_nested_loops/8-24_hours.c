408d9ed93e0442c7bee053d1eff0349b06e57382
Joshmyles <joshmyles844@gmail.com>
2023-09-16 02:01:42 -0700

    8. Federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,24 @@
 #include "main.h"
 
-/*comment*/void jack_bauer(void)/*comment*/
+/**
+ * jack_bauer - main function
+ * Return: nothing
+ */
+
+void jack_bauer(void)
 {
-int i,j;
-i=0;
-while(i<24)
+int i, j;
+i = 0;
+while (i < 24)
 {
-j=0;
-while(j<60)
+j = 0;
+while (j < 60)
 {
-_putchar((i/10)+'0');
-_putchar((i%10)+'0');
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
 _putchar(':');
-_putchar((j/10)+'0');
-_putchar((j%10)+'0');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 _putchar('\n');
 j++;
 }
