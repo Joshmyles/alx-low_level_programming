c6b458c33091ca83bf7aa8e9a653aeb7c4c87bf1
Joshmyles <joshmyles844@gmail.com>
2023-09-19 19:07:37 -0700

    Number 8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - the function
+ * @str: the input
+ * Return: the output
+ */
+
+void puts_half(char *str)
+{
+int length = strlen(str);
+int start;
+if (length % 2 == 0)
+{
+start = length / 2;
+}
+else
+{
+start = (length - 1) / 2;
+}
+for (int i = start; i < length; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
