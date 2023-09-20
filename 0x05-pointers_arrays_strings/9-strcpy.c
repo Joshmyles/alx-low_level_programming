508c76ac02abd0a4aee6f7163a0cbbda8de7295f
Joshmyles <joshmyles844@gmail.com>
2023-09-19 19:20:54 -0700

    Number 10

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - the function
+ * @dest: input
+ * @src: source
+ * Return: sth
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char *original_dest = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (original_dest);
+}
