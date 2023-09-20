b8199af86a16a42294a28bb1d96e280758a040e8
Joshmyles <joshmyles844@gmail.com>
2023-09-19 19:11:41 -0700

    Number 9

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * point_array - the function
+ * @a: input
+ * @n - input
+ * Return: output
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
