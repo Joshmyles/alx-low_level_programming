ed701acc6c55f4b04c1da3d0875041fa1c2e3524
Joshmyles <joshmyles844@gmail.com>
2023-09-21 20:00:43 -0700

    new push

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
+#include <main.h>
 
-char *rot13(char *str) {
-    char *ptr = str;
-    
-    while (*ptr != '\0') {
-        if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z')) {
-            char base = (*ptr >= 'a' && *ptr <= 'z') ? 'a' : 'A';
-            *ptr = ((*ptr - base + 13) % 26) + base;
-        }
-        ptr++;
-    }
-    
-    return str;
+/**
+ * *rot13 - the function
+ * @str: input
+ * Return: str
+ */
+
+char *rot13(char *str)
+{
+char *ptr = str;
+while (*ptr != '\0')
+{
+if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z'))
+{
+char base = (*ptr >= 'a' && *ptr <= 'z') ? 'a' : 'A';
+*ptr = ((*ptr - base + 13) % 26) + base;
+}
+ptr++;
+}
+return (str);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,37 @@
-#include <stdio.h>
+#include <main.h>
 
-char *leet(char *str) {
-    char *ptr = str;
-    
-    while (*ptr != '\0') {
-        if (*ptr == 'a' || *ptr == 'A') {
-            *ptr = '4';
-        } else if (*ptr == 'e' || *ptr == 'E') {
-            *ptr = '3';
-        } else if (*ptr == 'o' || *ptr == 'O') {
-            *ptr = '0';
-        } else if (*ptr == 't' || *ptr == 'T') {
-            *ptr = '7';
-        } else if (*ptr == 'l' || *ptr == 'L') {
-            *ptr = '1';
-        }
-        ptr++;
-    }
-    
-    return str;
+/**
+ * *leet - the function
+ * @str: input
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+char *ptr = str;
+while (*ptr != '\0')
+{
+if (*ptr == 'a' || *ptr == 'A')
+{
+*ptr = '4';
+}
+else if (*ptr == 'e' || *ptr == 'E')
+{
+*ptr = '3';
+}
+else if (*ptr == 'o' || *ptr == 'O')
+{
+*ptr = '0';
+}
+else if (*ptr == 't' || *ptr == 'T')
+{
+*ptr = '7';
+}
+else if (*ptr == 'l' || *ptr == 'L')
+{
+*ptr = '1';
+}
+ptr++;
+}
+return (str);
 }
