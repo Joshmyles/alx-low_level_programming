d6eeafe0d948eddef8083a2eaeea9c62093bc21e
Joshmyles <joshmyles844@gmail.com>
2023-10-10 11:49:00 -0700

    1. A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - the dog struct
+ * struct dog - the struct
  * @name: input
  * @age: input
  * @owner: input
@@ -13,6 +13,7 @@ struct dog
 char *name;
 float age;
 char *owner;
-};
+}
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
