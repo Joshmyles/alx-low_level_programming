94b3950cdc163df76baba7fbc0c0351c3f69b92a
Joshmyles <joshmyles844@gmail.com>
2023-10-10 12:14:32 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,18 +6,52 @@
  * @name: input
  * @age: input
  * @owner: input
+ *
+ * Description: Information on a dog
  */
-
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
+/**
+ * init_dog -function
+ * @d: input
+ * @name: input
+ * @age: input
+ * @owner: input
+ *
+ * Description: Prints dog details
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog -function
+ * @d: input
+ *
+ * Description: Prints dog details
+ */
 void print_dog(struct dog *d);
+
+/**
+ * new_dog -function
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Return: new dog
+ *
+ * Description: Prints dog details
+ */
 dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog -function
+ * @d: input
+ *
+ * Description: Prints dog details
+ */
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
