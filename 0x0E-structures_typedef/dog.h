710426dbc3350b373936f2ec43b42de001f4ec3f
Joshmyles <joshmyles844@gmail.com>
2023-10-10 11:44:26 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the dog struct
+ * @name: input
+ * @age: input
+ * @owner: input
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
