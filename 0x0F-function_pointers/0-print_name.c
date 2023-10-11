54801f33f4a2397223ba73a9f02d4e2666e6a137
Joshmyles <joshmyles844@gmail.com>
2023-10-11 08:40:32 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - the function
+ * @name: the input
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name)
+{
+printf("Name: %s\n", name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,5 @@
+0x0F-function_pointers
+
+Exercise on: Function pointers
+
+
