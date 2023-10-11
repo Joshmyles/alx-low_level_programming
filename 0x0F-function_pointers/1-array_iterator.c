dfa7ce48720d03f434958b2ce7466f0c55410608
Joshmyles <joshmyles844@gmail.com>
2023-10-11 11:51:27 -0700

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
@@ -11,7 +12,8 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-for (size_t i = 0; i < size; i++)
+size_t i;
+for (i = 0; i < size; i++)
 {
 action(array[i]);
 }
