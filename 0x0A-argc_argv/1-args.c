244a3e37cdb5040521f3831ca46f3906e4dc1fb8
Joshmyles <joshmyles844@gmail.com>
2023-10-23 04:49:52 -0700

    Start of the stringly linked list assignment

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the function
+ * @argc: input
+ * @argv: input
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
